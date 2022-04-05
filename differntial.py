6fb88502ad06642896ae7fa0ccfc675598c069b7
KIMYO9 <fgh33678@gmail.com>
2022-04-05 14:09:44 +0900

    a_x0.01

--- differntial.py
+++ differntial.py
@@ -1,6 +1,8 @@
 # value = input().split(' ')
 '''
-jisujisu- ax^n 함수가 아직 구현되지 않아 지수에 지수가 있을경우 지수의 미분값이 None이나오는 오류 생김
+jisujisu- ax^n 함수가 아직 구현되지 않아 지수에 지수가 있을경우 지수의 미분값이 None이나오는 오류 생김 
+-확인 x
+입력값 지수를 ()로 묶었을 때 지수의 지수를 슬라이싱 할떄 오류가 생김
 '''
 def checksign(value):#일반 스펙 곱미분법 분류 함수
     pass
@@ -29,7 +31,7 @@ def jisumibun(value): #value ex) 4^7x, log(1,x), 2x, e^4x+1 |ln은사용 x
                 return '0'
             elif '^' in jisu:#지수에 지수가 있을 때
                 jisujisu = jisumibun(jisu)
-                return f"{'-'if minusbool else ''}{jisujisu if jisujisu !='1'else ''}e^{jisu}"
+                return f"{'-'if minusbool else ''}{jisujisu if jisujisu !='1'else ''}e{'^('+jisu+')'}"
             else:#지수가 1차일때 
                 fjisu = jisu[:jisu.index('x')]
                 sangsu = val*int(fjisu)
@@ -38,16 +40,17 @@ def jisumibun(value): #value ex) 4^7x, log(1,x), 2x, e^4x+1 |ln은사용 x
         else:#a^x일때
             valuenum = [int(aa) for aa in value[:value.index('x')].split() if aa.isdigit()]
             val = valuenum[0]
-            if jisu.isdigit():
+            if value.isdigit():
                 return '0'
             # elif '^' in jisu:  #구현 조금 힘듦 4x^2x^2 = lnf(x) = 2x^2*ln4x
             #     jisujisu = jisumibun(jisu)
             #     return f"{'-'if minusbool else ''}{jisujisu if jisujisu != '1' else''}x^{jisu}"
             else:
-                fjisu = jisu[:jisu.index('x')]
+                fjisu = value[value.index('^')+1:]
                 sangsu = val*int(fjisu)
+                return f"{'-'if minusbool else ''}{sangsu if sangsu != 1 else ''}x{'^' + int(jisu)-1 if int(jisu)-1 != 1 else ''}"
 
-print(jisumibun('45e^7x'))
+print(jisumibun('45e^7x^2'))
 
 def triFunc(value):
     useValue = value.lower()
